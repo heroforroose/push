594df1f12d76a5f859fab56a3952df96b63c5a99
WULIN <673586831@qq.com>
2019-08-31 18:13:07 +0800

    project init

---
+++ index.ts
@@ -0,0 +1,4 @@
+import path = require("path");
+import fs = require("fs");
+let basename = path.posix.basename("G:\projects\achermind\push\index","ts");
+console.log(basename);
---
+++ res/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require("path");
+let basename = path.posix.basename("G:\projects\achermind\push\index", "ts");
+console.log(basename);
+//# sourceMappingURL=index.js.map
